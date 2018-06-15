19d48fc0f51ab9e32df3fcfcb98a4abe77170a77
ZAID SAEED <zsaee060@uottawa.ca>
2018-06-15 19:46:13 -0400

    added navbar

---
+++ src/components/Landing.js
@@ -0,0 +1,15 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Navbar from "./Navbar.js";
+
+const Landing = () => {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+};
+
+Landing.propTypes = {};
+
+export default Landing;
---
+++ src/components/Navbar.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+
+class Navbar extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+        <a className="navbar-brand" href="#">
+          Navbar
+        </a>
+        <button className="navbar-toggler">
+          <span className="navbar-toggler-icon" />
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarColor01">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="#">
+                Home <span className="sr-only">(current)</span>
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Features
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Pricing
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                About
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
