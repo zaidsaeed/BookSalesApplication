4f837b60a7d84d1ef09529e8555da67010ef49d6
ZAID SAEED <zsaee060@uottawa.ca>
2018-07-01 22:43:25 -0400

    added some changes to the application home page

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -7,18 +7,50 @@ const Landing = () => {
   let bookcards = protos.map((book, index) => {
     return <Book book={book} />;
   });
+  let newReleases = bookcards.slice(0, 5);
+  let bestSellers = bookcards.slice(5, 10);
+  let ourRecommendations = bookcards.slice(10, 15);
 
   return (
-    <div
-      style={{
-        display: "grid",
-        gridTemplateColumns: "repeat(5, 20rem [col-start])",
-        gridGap: "10px",
-        gridAutoFlow: "row",
-        padding: "15px"
-      }}
-    >
-      {bookcards}
+    <div>
+      <h1 style={{ margin: "15px 0px 0px 25px" }}>New Releases: </h1>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(5, 20rem [col-start])",
+          gridGap: "10px",
+          gridAutoFlow: "row",
+          padding: "0px 25px 15px 25px"
+        }}
+      >
+        {newReleases}
+      </div>
+
+      <h1 style={{ margin: "15px 0px 0px 25px" }}>Best Sellers: </h1>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(5, 20rem [col-start])",
+          gridGap: "10px",
+          gridAutoFlow: "row",
+          padding: "0px 25px 15px 25px"
+        }}
+      >
+        {bestSellers}
+      </div>
+
+      <h1 style={{ margin: "15px 0px 0px 25px" }}>Our Recommendations: </h1>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(5, 20rem [col-start])",
+          gridGap: "10px",
+          gridAutoFlow: "row",
+          padding: "0px 25px 15px 25px"
+        }}
+      >
+        {ourRecommendations}
+      </div>
     </div>
   );
 };
