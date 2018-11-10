5f5fd69c9f921b8d6da54059ef989b1d88627af4
ZAID SAEED <zsaee060@uottawa.ca>
2018-11-10 14:02:25 -0500

    trying to make app more respnsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,71 +14,65 @@ class Navbar extends Component {
             BookSales
           </a>
         </Link>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarColor01"
-          aria-controls="navbarColor01"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon" />
-        </button>
 
-        <div className="collapse navbar-collapse" id="navbarColor01">
+        <ul
+          className="navbar-nav mr-auto"
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            width: "matchContent"
+          }}
+        >
+          <li className="nav-item" style={{ marginRight: "5px" }}>
+            <Link to="/genre">
+              <a className="nav-link">Politics</a>
+            </Link>
+          </li>
+          <li className="nav-item" style={{ marginRight: "5px" }}>
+            <Link to="/genre">
+              <a className="nav-link">History</a>
+            </Link>
+          </li>
+          <li className="nav-item" style={{ marginRight: "5px" }}>
+            <Link to="/genre">
+              <a className="nav-link">Fiction</a>
+            </Link>
+          </li>
+          <li className="nav-item" style={{ marginRight: "5px" }}>
+            <Link to="/genre">
+              <a className="nav-link">Health</a>
+            </Link>
+          </li>
+          <li className="nav-item" style={{ marginRight: "5px" }}>
+            <Link to="/genre">
+              <a className="nav-link">Science</a>
+            </Link>
+          </li>
+        </ul>
+
+        <div
+          className="form-inline my-2 my-lg-0"
+          id="navbarColor01"
+          style={{
+            display: "flex",
+            alignContent: "center",
+            justifyItems: "center",
+            flexFlow: "row"
+          }}
+        >
+          <Search />
           <ul className="navbar-nav mr-auto">
             <li className="nav-item">
-              <Link to="/genre">
-                <a className="nav-link">Politics</a>
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/genre">
-                <a className="nav-link">History</a>
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/genre">
-                <a className="nav-link">Fiction</a>
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/genre">
-                <a className="nav-link">Health</a>
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/genre">
-                <a className="nav-link">Science</a>
+              <Link to="/shoppingcart">
+                <i
+                  style={{ marginRight: "75px", hover: "pointer" }}
+                  className="nav-link material-icons"
+                >
+                  shopping_cart
+                </i>
               </Link>
             </li>
           </ul>
-
-          <div
-            className="form-inline my-2 my-lg-0"
-            id="navbarColor01"
-            style={{
-              display: "flex",
-              alignContent: "center",
-              justifyItems: "center",
-              flexFlow: "row"
-            }}
-          >
-            <Search />
-            <ul className="navbar-nav mr-auto">
-              <li className="nav-item">
-                <Link to="/shoppingcart">
-                  <i
-                    style={{ marginRight: "75px", hover: "pointer" }}
-                    className="nav-link material-icons"
-                  >
-                    shopping_cart
-                  </i>
-                </Link>
-              </li>
-            </ul>
-          </div>
         </div>
       </nav>
     );
